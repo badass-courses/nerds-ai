6d8274fd98231f7ba1548284854dbc768958fed3
Mykola Bilokonsky <mbilokonsky@gmail.com>
2024-06-20 22:23:52 -0400

    tweaking guidance

--- src/prebuilt/graph/guidance/programming.ts
+++ src/prebuilt/graph/guidance/programming.ts
@@ -1,6 +1,6 @@
 import { GraphGuidance } from "../index.js";
 
 export const programming_graph_guidance: GraphGuidance = {
-  vertex_labels: ["ProgrammingLanguage", "Framework", "Technique", "Strategy", "Tip", "Problem", "Class", "Type", "Pattern"],
-  edge_labels: ["uses", "solves", "causes", "prevents", "improves", "implements", "takes_type", "returns_type"]
+  vertex_labels: ["ProgrammingLanguage", "Framework", "Technique", "Keyword", "Strategy", "Tip", "Problem", "Class", "Type", "Pattern"],
+  edge_labels: ["solves", "causes", "prevents", "improves", "implements", "takes_type", "returns_type"]
 }
